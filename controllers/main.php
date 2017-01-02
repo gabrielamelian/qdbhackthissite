08effb7014812828e83aea74061cfc4ae77fb903
gabrielamelian <gabrielamelian@gmail.com>
2017-01-02 20:21:50 +1300

    Adds basic test

---
+++ controllers/main.php
@@ -0,0 +1,14 @@
+<?php
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+namespace Controllers
+{
+    class Main
+    {
+        public function bar(Request $request, Application $app) {
+
+        }
+    }
+}
--- web/index.php
+++ web/index.php
@@ -4,10 +4,21 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+require_once __DIR__.'/../config.php';
+require_once __DIR__.'/../controllers/main.php';
+
 $app['debug'] = true;
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
+
+// $app->get('/blog/{id}', function ($id) use ($app) {
+//     $sql = "SELECT * FROM qdb_quotes WHERE id = ?";
+//     $post = $app['db']->fetchAssoc($sql, array((int) $id));
+//
+//     return  "<h1>{$post['title']}</h1>".
+//             "<p>{$post['body']}</p>";
+// });
 
 $app->run();
