b4353ef34bb334d073c1b72c0fb0453c956a739e
gabrielamelian <gabrielamelian@gmail.com>
2017-01-04 22:39:54 +1300

    Test hello world passing

---
+++ app/app.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+require_once __DIR__.'/../config.php';
+require_once __DIR__.'/../controllers/main.php';
+
+$app['debug'] = true;
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return '<html>Hello '.$app->escape($name).'</html>';
+});
+
+return $app;
