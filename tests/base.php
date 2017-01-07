d858a9f8dc68856e3371bd071548b1d7d0f17c82
Pedro Worcel <pedro@worcel.com>
2017-01-06 21:52:31 -0500

    Add database mocking for unit tests.

---
+++ tests/base.php
@@ -0,0 +1,58 @@
+<?php
+
+use Silex\WebTestCase;
+
+class BaseTest extends WebTestCase {
+
+    private $testDB = 'TEST_DB_QDB';
+    public $db = NULL;
+
+    public function createApplication() {
+        define('TESTING', true);
+
+        $app = require __DIR__.'/../app/app.php';
+        $app['debug'] = true;
+        unset($app['exception_handler']);
+
+        $this->db = $app['db'];
+
+        return $app;
+    }
+
+    public function setUp() {
+        parent::setUp();
+        $this->databaseSetUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+        $this->databaseTearDown();
+    }
+
+    public function databaseSetUp($last = false) {
+        try {
+            $this->db->query("CREATE DATABASE {$this->testDB};");
+            $this->db->query("USE {$this->testDB};");
+        } catch(PDOException $exc) {
+            // If temporary database already exists, delete and try again.
+            if(!$last) {
+                $this->databaseTearDown();
+                $this->databaseSetUp(true);
+            } else {
+                throw $exc;
+            }
+        }
+
+        $this->initSQL(__DIR__.'/../schema.sql');
+        $this->initSQL(__DIR__.'/seeds.sql');
+    }
+
+    public function databaseTearDown() {
+        $this->db->query("DROP DATABASE {$this->testDB}");
+    }
+
+    public function initSQL($file) {
+        $sql = file_get_contents($file);
+        $this->db->query($sql);
+    }
+}
