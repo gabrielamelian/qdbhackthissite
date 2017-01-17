5e39903c086484fe9f4c9a316e2360050cb73f29
gabrielamelian <gabrielamelian@gmail.com>
2017-01-17 22:18:33 +1300

    Adds new test for quotes number passing

--- tests/RandomTest.php
+++ tests/RandomTest.php
@@ -21,6 +21,7 @@ class IndexRandom extends Base {
 
       $this->assertTrue($client->getResponse()->isOk(), 'Response from quotes random is ok');
       $this->assertCount(50, $crawler->filter('div.quotebox'), 'Page contains 50 divs with the class "quotebox"');
+      $this->assertCount(50, $crawler->filter('p.quotetext'), 'Page contains 50 p tags with the class "quotetext"');
     }
 
 }
