4834bc05bae7b492bfdacdb85c15fdb1f85d18ef
gabrielamelian <gabrielamelian@gmail.com>
2017-02-17 21:51:24 +1300

    Creates test for bottom 50 route

---
+++ tests/Bottom50Test.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../includes/BaseTest.php';
+
+class Bottom50 extends BaseTest {
+
+    public function testBottom50() {
+      $this->seedDB();
+      $client = $this->createClient();
+      $crawler = $client->request('GET', '/quotes/bottom50');
+
+      $this->assertTrue($client->getResponse()->isOk(), 'Response from quotes bottom50 is ok');
+      $this->assertCount(50, $crawler->filter('div.quotebox'), 'Page contains 50 divs with the class "quotebox"');
+      $this->assertCount(50, $crawler->filter('p.quotetext'), 'Page contains 50 p tags with the class "quotetext"');
+      $this->assertCount(0, $crawler->filter('p.quotetext:contains("This is a quote that has not been approved")'));
+    }
+
+}
